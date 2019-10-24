0269774c4a5f35f5c1696db9e08df49ec08f6e0a
lasarian <anonimduma@gmail.com>
2019-10-24 22:38:18 +0300

    editing psd 2

--- resources/views/psd2/header.blade.php
+++ resources/views/psd2/header.blade.php
@@ -18,7 +18,7 @@
                 </ul>
             </div>
             <div class="col-auto">
-                <ul class="m-0 p-0 social-logo" >
+                <ul class="m-0 p-0 social-logo">
                     <li class="rss">
                         <a href="#"></a>
                     </li>
@@ -45,5 +45,43 @@
 
 
 <nav class="navbar header navbar-expand-lg navbar-light">
-
+    <a class="navbar-brand" href="#"> <img src="/images/logo.png" alt=""></a>
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span>go to home</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features <span>discover</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Portfolio <span> our works</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products <span>the showcase</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Company blog <span>latest news & updates</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact us <span>keep in touch</span></a>
+                </li>
+                <li class="nav-item">
+                    <div class="input-group">
+                        <input class="form-control py-2" type="search" value="search" id="example-search-input">
+                        <span class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
--- resources/views/psd2/index.blade.php
+++ resources/views/psd2/index.blade.php
@@ -2,6 +2,65 @@
 
 @section('content')
  <div class="home">
+    <div class="container">
+        <div class="articles">
+            <div class="row">
+                <div class="col-md-4 phone">
+                    <h6>Responsive Theme</h6>
 
+                    <div>Lorem ipsum dolor sit amet, consectetur addipiscing eli. Curabitur euismod enim a metus adipiscing aliquam. Vestibulum in vestibulum lore. Morbi imperdiet velit eu arcu consequat. sit amet tristique enim ultrivies. Aenean faucibus duit sit amet elit iaculis iaculis. Nullam colutpat facilisis eros sed commodo.</div>
+                </div>
+                <div class="col-md-4 settings">
+                    <h6>Responsive Theme</h6>
+
+                    <div>Lorem ipsum dolor sit amet, consectetur addipiscing eli. Curabitur euismod enim a metus adipiscing aliquam. Vestibulum in vestibulum lore. Morbi imperdiet velit eu arcu consequat. sit amet tristique enim ultrivies. Aenean faucibus duit sit amet elit iaculis iaculis. Nullam colutpat facilisis eros sed commodo.</div>
+                </div>
+                <div class="col-md-4 magic">
+                    <h6>Responsive Theme</h6>
+
+                    <div>Lorem ipsum dolor sit amet, consectetur addipiscing eli. Curabitur euismod enim a metus adipiscing aliquam. Vestibulum in vestibulum lore. Morbi imperdiet velit eu arcu consequat. sit amet tristique enim ultrivies. Aenean faucibus duit sit amet elit iaculis iaculis. Nullam colutpat facilisis eros sed commodo.</div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="quote">
+            <h3>
+                <i class="fas fa-quote-left"></i>
+                Donec quam felis, ultrivies nec, pellentesque eu, pretium quis, sem. Nulla massa quis enim. Donec pede justo, fringilla vel, alquet nec, vulputate eget, arcu.
+                <i class="fas fa-quote-right"></i>
+
+                <h5>John Doe - CEO <a href="#"> Envato</a></h5>
+            </h3>
+        </div>
+
+        <div class="row">
+            <div class="col-md-9">
+                <div class="title">
+                    <h4>Top five reasons to buy Rt-theme 18</h4> <div><span></span></div>
+                </div>
+                <div>Nam quam nunv, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus . Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt</div>
+            </div>
+            <div class="col-md-3">
+                <div class="news">
+                    <h4>Latest news </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+     <div class="purchase">
+         <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                    <h6>Nam quam nunc, blandit vel, luctus ppulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.</h6>
+                </div>
+                <div class="col-md-auto">
+                    <div class="btn btn-purchase">Purchase Now</div>
+                </div>
+            </div>
+         </div>
+     </div>
  </div>
 @endsection
